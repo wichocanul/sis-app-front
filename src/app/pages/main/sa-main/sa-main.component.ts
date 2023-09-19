ae4e447b513413bd3d282f3f4530555f73336852
wicho <wichocanul07@gmail.com>
2023-09-19 14:40:36 -0600

    SAPF: Improve sa main

--- src/app/pages/main/sa-main/sa-main.component.ts
+++ src/app/pages/main/sa-main/sa-main.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DarkLightModeService } from 'src/app/services/darkLightMode/dark-light-mode.service';
 
 @Component({
   selector: 'app-sa-main',
   templateUrl: './sa-main.component.html',
   styleUrls: ['./sa-main.component.css']
 })
-export class SaMainComponent {
+export class SaMainComponent implements OnInit {
 
+  lightModeEnabled: boolean = true;
   sidebarHidden: boolean = true;
+
+  constructor(
+    private darkLightmode: DarkLightModeService,
+  ) {}
+
+  ngOnInit(): void {
+    this.darkLightmode.lightModeEnabled$.subscribe( status => {
+      this.lightModeEnabled = status;
+    })
+  }
 
   statusMenu(status: boolean) {
     this.sidebarHidden = status;
--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,15 +1,27 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { DarkLightModeService } from 'src/app/services/darkLightMode/dark-light-mode.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
+  lightModeEnabled: boolean = true;
   menuEnabled: boolean = false;
 
   @Output() statusMenu = new EventEmitter<boolean>();
+
+  constructor(
+    private darkLightmode: DarkLightModeService,
+  ) {}
+
+  ngOnInit(): void {
+    this.darkLightmode.lightModeEnabled$.subscribe( status => {
+      this.lightModeEnabled = status;
+    })
+  }
 
   changeStatusMenu() {
     this.statusMenu.emit(this.menuEnabled);
