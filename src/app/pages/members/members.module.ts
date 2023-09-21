3fbbf292e82e532b98e0c7ba0be302839242d8a6
wicho <wichocanul07@gmail.com>
2023-09-20 20:40:42 -0600

    SAPF: Add global styles and create dont-work component

--- src/app/pages/members/members.module.ts
+++ src/app/pages/members/members.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MembersMainComponent } from './members-main/members-main.component';
 import { WorkTeamComponent } from './work-team/work-team.component';
+import { DontWorkComponent } from './dont-work/dont-work.component';
 
 
 
 @NgModule({
   declarations: [
     MembersMainComponent,
-    WorkTeamComponent
+    WorkTeamComponent,
+    DontWorkComponent
   ],
   imports: [
     CommonModule
