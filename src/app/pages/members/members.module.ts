0c840ff1ba244c9a36d19b9b5e64fe60c2649d1c
wicho <wichocanul07@gmail.com>
2023-09-20 19:13:49 -0600

    SAPF: Create work team component and add styles

--- src/app/pages/members/members.module.ts
+++ src/app/pages/members/members.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MembersMainComponent } from './members-main/members-main.component';
+import { WorkTeamComponent } from './work-team/work-team.component';
 
 
 
 @NgModule({
   declarations: [
-    MembersMainComponent
+    MembersMainComponent,
+    WorkTeamComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/pages/members/work-team/work-team.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-team',
+  templateUrl: './work-team.component.html',
+  styleUrls: ['./work-team.component.css']
+})
+export class WorkTeamComponent {
+
+}
