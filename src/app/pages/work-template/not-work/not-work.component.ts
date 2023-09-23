30257124e303dba346b2c61610b8d0be58f4d483
wicho <wichocanul07@gmail.com>
2023-09-22 21:20:39 -0600

    SAPF: create work template with styles

---
+++ src/app/pages/work-template/not-work/not-work.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-work',
+  templateUrl: './not-work.component.html',
+  styleUrls: ['./not-work.component.css']
+})
+export class NotWorkComponent {
+
+}
--- src/app/pages/work-template/work-template.module.ts
+++ src/app/pages/work-template/work-template.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkTemplateMainComponent } from './work-template-main/work-template-main.component';
 import { RepresentativesComponent } from './representatives/representatives.component';
+import { WorkTeamComponent } from './work-team/work-team.component';
+import { NotWorkComponent } from './not-work/not-work.component';
 
 
 
 @NgModule({
   declarations: [
     WorkTemplateMainComponent,
-    RepresentativesComponent
+    RepresentativesComponent,
+    WorkTeamComponent,
+    NotWorkComponent
   ],
   imports: [
     CommonModule
