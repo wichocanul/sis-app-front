b30da564d0bcb0e9f460d0fb917ffff4f3c83a45
wicho <wichocanul07@gmail.com>
2023-09-23 20:51:14 -0600

    SAPF: Improve citizens-modal with input text type

--- src/app/components/citizens-modal/citizens-modal.component.ts
+++ src/app/components/citizens-modal/citizens-modal.component.ts
@@ -8,7 +8,9 @@ import { Component, Input } from '@angular/core';
 export class CitizensModalComponent {
 
   searchCitizen: string = ' ';
+  subtitle: string = 'hola';
   modalEnabled: boolean = false;
+  @Input() titleButton: string = '';
   @Input() titleModal: string = '';
 
   statusModal() {
