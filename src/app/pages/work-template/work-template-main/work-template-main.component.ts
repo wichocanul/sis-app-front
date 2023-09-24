cd13adba58b459721c289150821f6ddf83332db1
wicho <wichocanul07@gmail.com>
2023-09-23 21:22:46 -0600

    SAPF: Create end period function with alert

--- src/app/pages/work-template/work-template-main/work-template-main.component.ts
+++ src/app/pages/work-template/work-template-main/work-template-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DarkLightModeService } from 'src/app/services/darkLightMode/dark-light-mode.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-work-template-main',
@@ -8,7 +9,7 @@ import { DarkLightModeService } from 'src/app/services/darkLightMode/dark-light-
 })
 export class WorkTemplateMainComponent implements OnInit {
 
-  currentPeriod : boolean = false;
+  currentPeriod : boolean = true;
   lightModeEnabled: boolean = true;
 
   constructor(
@@ -19,6 +20,28 @@ export class WorkTemplateMainComponent implements OnInit {
     this.darkLightmode.lightModeEnabled$.subscribe( status => {
       this.lightModeEnabled = status;
     })
+  }
+
+  endPeriod() {
+    Swal.fire({
+      title: 'Estas seguro de terminar el Periodo?',
+      text: "Al terminar el periodo ya no podras hacer cambios",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Terminar'
+    }).then((result) => {
+      if(result.isConfirmed) {
+        Swal.fire(
+          'Terminado Correctamente!',
+          'El periodo se ha terminado',
+          'success'
+        )
+        this.currentPeriod = false;
+      }
+    })
+
   }
 
 }
