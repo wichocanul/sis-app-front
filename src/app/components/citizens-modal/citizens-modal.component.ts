8b3a9b359015140757da0965e68c5dc806bdbb93
wicho <wichocanul07@gmail.com>
2023-09-25 13:41:09 -0600

    SAPF: Create citizen modal window with functions

--- src/app/components/citizens-modal/citizens-modal.component.ts
+++ src/app/components/citizens-modal/citizens-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { DarkLightModeService } from 'src/app/services/darkLightMode/dark-light-mode.service';
 
 @Component({
   selector: 'app-citizens-modal',
@@ -7,14 +8,30 @@ import { Component, Input } from '@angular/core';
 })
 export class CitizensModalComponent {
 
-  searchCitizen: string = ' ';
+  searchCitizen: string = '';
   subtitle: string = 'hola';
   modalEnabled: boolean = false;
+  lightModeEnabled: boolean = true;
   @Input() titleButton: string = '';
   @Input() titleModal: string = '';
 
+  constructor(
+    private darkLightmode: DarkLightModeService,
+  ) {}
+
+  ngOnInit(): void {
+    this.darkLightmode.lightModeEnabled$.subscribe( status => {
+      this.lightModeEnabled = status;
+    })
+  }
+
   statusModal() {
     this.modalEnabled = !this.modalEnabled;
+  }
+
+  changeName(name: string) {
+    console.log(name)
+    this.searchCitizen = name;
   }
 
 }
